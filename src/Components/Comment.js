46a3fa000beb180f3b8683a71f4f0d6cd90cfdd4
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-06-27 08:45:14 +0900

    2 files modified

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -3,15 +3,17 @@ import React from 'react';
 const image =
   'https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreeicons.io%2Fessential-web-2%2Fuser-ciecle-round-account-person-icon-40275&psig=AOvVaw1PJHbIgN-rIMzZQDwj5Lcp&ust=1687827738423000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIj3l73e3_8CFQAAAAAdAAAAABAD';
 
-const Comment = ({ data }) => {
-  const { name, text, replies } = data;
-
+const Comment = ({ comment }) => {
   return (
     <div className="flex shadow-lg rounded-lg">
       <img className="h-10 w-10 mx-2 mt-1" src={image}></img>
       <div className="px-3">
-        <p className="font-bold">Name: {name}</p>
-        <p>Comment: {text}</p>
+        <p className="font-bold">
+          Name: {comments?.snippet?.topLevelComment?.snippet?.authorDisplayName}
+        </p>
+        <p>
+          Comment: {comments?.snippet?.topLevelComment?.snippet?.textDisplay}
+        </p>
       </div>
     </div>
   );
--- src/Components/CommentsContainer.js
+++ src/Components/CommentsContainer.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useSearchParams } from 'react-router-dom';
 import Comment from './Comment';
 
 const commetnsData = [
@@ -53,10 +55,35 @@ const CommentsLis = ({ data }) => {
 };
 
 const CommentsContainer = () => {
+  const [comments, setComments] = useState({});
+  const COMMENTS_API =
+    'https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyC3oHmoZpZ7GWo6V5iZHgOJ6cQRecy4NtY&textFormat=plainText&part=snippet&videoId=';
+
+  const [searchParams] = useSearchParams();
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  const getComments = async () => {
+    const data = await fetch(
+      'https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyC3oHmoZpZ7GWo6V5iZHgOJ6cQRecy4NtY&textFormat=plainText&part=snippet&videoId=' +
+        searchParams.get('v')
+    );
+    const json = await data.json();
+
+    setComments(json.items);
+  };
+
   return (
     <div className="m-5 p-2">
       <h1 className="font-bold">Comments</h1>
-      <CommentsLis data={commetnsData} />
+
+      {comments?.map((comments) => (
+        <Comment comment={comments} />
+      ))}
+
+
+
     </div>
   );
 };
