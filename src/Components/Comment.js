a7985dbdd5c9beb605e338d37dce031a47b4cfa8
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-06-27 08:50:35 +0900

    2 files modified

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const image =
   'https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreeicons.io%2Fessential-web-2%2Fuser-ciecle-round-account-person-icon-40275&psig=AOvVaw1PJHbIgN-rIMzZQDwj5Lcp&ust=1687827738423000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIj3l73e3_8CFQAAAAAdAAAAABAD';
 
-const Comment = ({ comment }) => {
+const Comment = ({ comments }) => {
   return (
     <div className="flex shadow-lg rounded-lg">
       <img className="h-10 w-10 mx-2 mt-1" src={image}></img>
--- src/Components/CommentsContainer.js
+++ src/Components/CommentsContainer.js
@@ -1,89 +1,29 @@
-import React, { useState, useEffect } from 'react';
-
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import Comment from './Comment';
+import { useParams, useSearchParams } from 'react-router-dom';
 
-const commetnsData = [
-  {
-    name: 'Abhikesh',
-    text: 'lorem unasfsdfg ad ',
-    replies: [],
-  },
-  {
-    name: 'Abhikesh',
-    text: 'lorem unasfsdfg ad ',
-    replies: [],
-  },
-  {
-    name: 'Abhikesh1',
-    text: 'lorem unasfsdfg ad ',
-    replies: [
-      {
-        name: 'Abhikesh12',
-        text: 'lorem unasfsdfg ad ',
-        replies: [],
-      },
-      {
-        name: 'Abhikesh122',
-        text: 'lorem unasfsdfg ad ',
-        replies: [],
-      },
-      {
-        name: 'Abhikesh11',
-        text: 'lorem unasfsdfg ad ',
-        replies: [
-          {
-            name: 'Abhikesh111',
-            text: 'lorem unasfsdfg ad ',
-            replies: [],
-          },
-        ],
-      },
-    ],
-  },
-];
-
-const CommentsLis = ({ data }) => {
-  return data.map((data, index) => (
-    <div>
-      <Comment key={index} data={data} />
-      <div className="pl-5 border-l-2">
-        <CommentsLis data={data.replies} />
-      </div>
-    </div>
-  ));
-};
+const COMMENT_API =
+  'https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=_VB39Jo8mAQ&key=AIzaSyC3oHmoZpZ7GWo6V5iZHgOJ6cQRecy4NtY&videoId=';
 
 const CommentsContainer = () => {
-  const [comments, setComments] = useState({});
-  const COMMENTS_API =
-    'https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyC3oHmoZpZ7GWo6V5iZHgOJ6cQRecy4NtY&textFormat=plainText&part=snippet&videoId=';
-
-  const [searchParams] = useSearchParams();
   useEffect(() => {
     getComments();
   }, []);
 
+  const [comments, setComments] = useState([]);
+  const [searchParams] = useSearchParams();
   const getComments = async () => {
-    const data = await fetch(
-      'https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyC3oHmoZpZ7GWo6V5iZHgOJ6cQRecy4NtY&textFormat=plainText&part=snippet&videoId=' +
-        searchParams.get('v')
-    );
+    const data = await fetch(COMMENT_API + searchParams.get('v'));
     const json = await data.json();
-
-    setComments(json.items);
+    setComments(json?.items);
   };
 
   return (
     <div className="m-5 p-2">
       <h1 className="font-bold">Comments</h1>
-
       {comments?.map((comments) => (
-        <Comment comment={comments} />
+        <Comment comments={comments} />
       ))}
-
-
-
     </div>
   );
 };
